da59be5a76d22cedac53050efdeacbf8dc54da43
Brandon Green <brandon@brandonleeproductions.com>
2018-05-24 03:25:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,8 @@ def line(line) # This is accepting the empty katz_deli array and puts'ing the st
   else
     output = "The line is currently: "
     count = 0 
-    line.each do |name|
-      count += 1 
+    line.each do |name| # This line is saying "do this for each index (name) in the line array"
+      count += 1 # increase count by one
       output += "#{count}. #{person}"
       output += " " if count != line.size
     end
@@ -18,7 +18,7 @@ end
 
 
 def take_a_number(line, name)
-  puts "Welcome, #{name}. You are number #{line.size + 1} in line."
+  puts "Welcome, #{name}. You are number #{line.size + 1} in line." # Adds 1 to the line index since people start counting from 1, not 0.
   line << name
 end
 
@@ -26,7 +26,7 @@ def now_serving(line)
   if line.size == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line.shift}."
+    puts "Currently serving #{line.shift}." #Removes the first name from line 
     line
   end
 end
