f4479d40d3ceabbaa4a21937ac7dd1aaef45cc52
Brandon Green <brandon@brandonleeproductions.com>
2018-05-24 02:52:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 katz_deli = []
 
-def line(katz_deli)
+def line(katz_deli) # This is accepting the empty katz_deli array and puts'ing the string.
   puts "The line is currently empty."
 end
 
-def take_a_number(katz_deli)
+def take_a_number(katz_deli, name)
   
 end
 
