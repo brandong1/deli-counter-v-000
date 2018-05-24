8d675d8f1ea212821af0252aff27128afdfdefb6
Brandon Green <brandon@brandonleeproductions.com>
2018-05-24 03:03:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,12 +2,14 @@ katz_deli = []
 
 def line(katz_deli) # This is accepting the empty katz_deli array and puts'ing the string.
   puts "The line is currently empty."
+  if array.count > 0 
+    
 
 end
 
 def take_a_number(katz_deli, name)
   name = gets.strip
-  puts "Welcome, #{name}. You are num"
+  puts "Welcome, #{name}. You are number #{array} in line."
 end
 
 def now_serving(katz_deli)
